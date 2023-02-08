5a9ea8090e6f5a7d573a560c5a3d9914ff542ec7
Josef Hrebec <josefhrebec02@gmail.com>
2023-02-08 15:25:22 +0100

    Initalize utils

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.API_LOCAL,
+  authDomain: "to-do-plus-89c79.firebaseapp.com",
+  projectId: "to-do-plus-89c79",
+  storageBucket: "to-do-plus-89c79.appspot.com",
+  messagingSenderId: "897685110014",
+  appId: "1:897685110014:web:011e0ae2d3b8dc94a9f9af",
+};
+
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <>
+      <GlobalStyle />
+      <App />
+    </>
+  </React.StrictMode>
+);
