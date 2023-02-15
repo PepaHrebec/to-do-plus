bee960b54f83318a7d66fa0a776b28755bd4a4d0
Josef Hrebec <josefhrebec02@gmail.com>
2023-02-15 14:13:20 +0100

    Implement basic inp form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,49 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function FormComp() {
-  return <></>;
+  const [taskName, setTaskName] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+  const [taskValue, setTaskValue] = useState("");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log({ taskName, taskDate, taskValue });
+  };
+
+  return (
+    <>
+      <form action="">
+        <label htmlFor="nameInp">Task:</label>
+        <input
+          type="text"
+          id="nameInp"
+          name="nameInp"
+          onChange={(e) => {
+            setTaskName(e.target.value);
+          }}
+        />
+        <label htmlFor="dateInp">When:</label>
+        <input
+          type="date"
+          id="dateInp"
+          name="dateInp"
+          onChange={(e) => {
+            setTaskDate(e.target.value);
+          }}
+        />
+        <label htmlFor="valueInp">What:</label>
+        <input
+          type="text"
+          id="valueInp"
+          name="valueInp"
+          onChange={(e) => {
+            setTaskValue(e.target.value);
+          }}
+        />
+        <button onClick={submitForm}>Submit</button>
+      </form>
+    </>
+  );
 }
 
 export { FormComp };
